1dfbb0b1fd805a008cd154e748d99d5642c2a2d0
GabrieleSgroi <79085594+GabrieleSgroi@users.noreply.github.com>
2022-01-06 17:33:35 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Setup for genetic_algorithm
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Genetic algorithm',
+    version='0.0.0',
+    packages=['genetic_alg'],
+    install_requires=[
+       'numpy',
+       'tensorflow',
+       'matplotlib',
+       'time',
+       'os',    
+    ]
+)
