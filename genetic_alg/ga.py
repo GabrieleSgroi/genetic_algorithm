397f2bf3e08db599b1ad0cd81267840c7d5815a1
GabrieleSgroi <79085594+GabrieleSgroi@users.noreply.github.com>
2022-01-20 13:10:35 +0100

    Update ga.py

--- genetic_alg/ga.py
+++ genetic_alg/ga.py
@@ -101,8 +101,10 @@ class GeneticAlg():
                          \nFitness mean:{},\nFitness std:{},\
                          \nAll-time best fitness:{}.'.format(
                           self.fitness_hist[-1][1],
-                          self.fitness_hist[-1][2], self.fitness_hist[-1][3]).
+                          self.fitness_hist[-1][2], 
+                          self.fitness_hist[-1][3],
                           self.best_fitness)
+                          )
                   print('Elapsed Time:{}h:{}m:{}s, Estimated to completion:{}h,{}m, {}s'.format(int(h),int(m),int(s),
                                                 int(he),int(me),int(se)))
                   if plot_fitness:
